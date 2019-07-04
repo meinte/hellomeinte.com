f5f3c33d816f1fcaea77b7c56085ad84353f4e28
Meinte Van't Kruis <meinte@gmail.com>
2019-07-04 17:45:12 +1000

    Print styling. Looping through work items. Ghostified other content

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const debug = require("debug")("hellomeinte:main");
 const error = require("debug")("hellomeinte:error");
 const GhostContentAPI = require("@tryghost/content-api");
+const handleBars = require("handlebars");
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
@@ -10,6 +11,18 @@ const apiKey = process.env.GHOST_API_KEY;
 const port = process.env.PORT;
 
 debug("starting..");
+
+handleBars.registerHelper("clientTitleEmphasize", clientTitle => {
+  const split = clientTitle.split(" ");
+  return `${split[0]}<em>${split[1]}</em>`;
+});
+
+handleBars.registerHelper("clientTags", tags =>
+  tags
+    .filter(tag => tag.visibility === "public")
+    .map(tag => tag.name)
+    .join(" | ")
+);
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
@@ -21,7 +34,17 @@ const api = new GhostContentAPI({
   version: "v2"
 });
 
-const WEBSITE_CONTENT_IDS = ["intro1"];
+//Maps ghost title to handlebars IDs
+const WEBSITE_CONTENT_IDS = {
+  intro1: "intro1",
+  contact_intro: "contact_intro",
+  work_intro: "work_intro",
+  "client: QGC": "qgc",
+  "client: Macquarie University": "macquarie",
+  "client: Deloitte": "deloitte",
+  other_work: "other_work",
+  about: "about"
+};
 
 const filterOnGhostTitle = (pages, title) =>
   pages.find(page => page.title === title);
@@ -29,7 +52,8 @@ const filterOnGhostTitle = (pages, title) =>
 const grabGhostContent = () => {
   return api.pages
     .browse({
-      fields: "html,title",
+      fields: "html,title,custom_excerpt,feature_image",
+      include: "tags",
       limit: "all",
       filter: "tag:hash-website",
       order: "title ASC"
@@ -41,10 +65,11 @@ const grabGhostContent = () => {
        * In this case, titles are stored in the WEBSITE_CONTENT_IDS array.
        * Content is then mapped(reduced) in an object through its title
        */
-      return WEBSITE_CONTENT_IDS.reduce((acc, title) => {
+      return Object.keys(WEBSITE_CONTENT_IDS).reduce((acc, title) => {
+        const ghostContent = filterOnGhostTitle(pages, title) || {};
         return {
           ...acc,
-          [title]: filterOnGhostTitle(pages, title).html
+          [WEBSITE_CONTENT_IDS[title]]: ghostContent
         };
       }, {});
     })
@@ -56,7 +81,19 @@ const grabGhostContent = () => {
 app.get("/", function(req, res) {
   grabGhostContent()
     .then(content => {
-      debug(content);
+      content.clients = Object.keys(content).reduce((acc, itemKey) => {
+        const item = content[itemKey];
+        const arr = [...acc];
+        if (item.title.indexOf("client") > -1) {
+          arr.push(item);
+          delete content[itemKey];
+        }
+        return arr;
+      }, []);
+      return content;
+    })
+    .then(content => {
+      debug("mapped content: ", content);
       res.render("home", content);
     })
     .catch(err => {
